bebfb7df7bfa8e2e84d5c80f52e17333ce6e36fb
namzezz <96433032+namzezz@users.noreply.github.com>
2024-05-27 16:26:57 +0530

    Time: 11 ms (8.87%) | Memory: 18.2 MB (95.57%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -15,22 +15,24 @@ public:
          ListNode* temp = dummyNode;
 
         while(list1!=NULL && list2!=NULL){
-            if(list1->val<list2->val){
+            if(list1->val<=list2->val){
                 temp->next=list1;
                 list1=list1->next;
+               
             }else{
                 temp->next=list2;
                 list2=list2->next;
             }
             temp=temp->next;
+            
         }
 
-        if(list1==NULL){
-            temp->next=list2;
-        }else{
+        if(list1!=NULL){
             temp->next=list1;
+        }else{
+            temp->next=list2;
         }
+            
         return dummyNode->next;
     }
-    
 };
