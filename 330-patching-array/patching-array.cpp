c33221b14c52b547f3d823f356b45515da5a9126
namzezz <96433032+namzezz@users.noreply.github.com>
2024-06-16 19:07:09 +0530

    Time: 9 ms (21.31%) | Memory: 13.6 MB (97.87%) - LeetSync

---
+++ 330-patching-array/patching-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long int currentSum = 1;
+        int patches = 0, index = 0;
+
+        while(currentSum <= n){
+            if(index < nums.size()  &&  nums[index] <= currentSum){
+                currentSum += nums[index];
+                index++;
+            } else {
+                currentSum += currentSum;
+                patches++;
+            }
+        }
+
+        return patches;
+    }
+};
