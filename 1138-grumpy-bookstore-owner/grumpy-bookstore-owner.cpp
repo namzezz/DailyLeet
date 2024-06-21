150782eafcce0b97517be96ea5e64fddfdbca71c
namzezz <96433032+namzezz@users.noreply.github.com>
2024-06-21 19:47:05 +0530

    Time: 383 ms (5.01%) | Memory: 34.4 MB (16.52%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -13,6 +13,7 @@ public:
 
         for (int i = 0; i <= customers.size() - minutes; i++) {
             for (int j = i; j < i + minutes; j++) {
+                //if he wasnt grumpy at this index then additional sum ye hota
                 if (grumpy[j] == 1) {
                     rsum += customers[j];
                 }
